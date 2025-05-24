98cc803cdcbd37f0c47dd502e6f94ca4c6bd2763
Oscar <ocook59@gmail.com>
2025-05-23 17:12:41 -0700

    refactored code and added button redirection.

---
+++ scripts/header-sidebar-buttons.js
@@ -0,0 +1,9 @@
+document.querySelector('.js-graph-editor-button').addEventListener('click', () => {
+    location.href='graph-editor.html';
+});
+document.querySelector('.js-home-button').addEventListener('click', () => {
+    location.href='home-page.html';
+});
+document.querySelector('.js-about-button').addEventListener('click', () => {
+    location.href='about-page.html';
+});
